333fd9436e903e30ab9ace36aff1d87e979e28a1
beescuit9510 <pengsoon20@gmail.com>
2022-11-17 14:41:48 +0900

    feat : undirected graph, connected components count problem with graph traversal ( DFS )

---
+++ tree/graph/connected_components_count.py
@@ -0,0 +1,60 @@
+def has_path_dfs(graph):
+    component_count = 0
+    visited_node = []
+
+    for node in graph.keys():
+
+        if node in visited_node:
+            continue
+
+        component_count += 1
+
+        stack = [node]
+
+        while stack:
+            current_node = stack.pop()
+            visited_node.append(current_node)
+
+            for neighbor in graph[current_node]:
+                if neighbor not in visited_node:
+                    stack.append(neighbor)
+
+    return component_count
+
+
+def has_path_recursive_dfs(graph):
+    component_count = 0
+    visited_node = []
+
+    for node in graph.keys():
+        component_count += explore(graph, node, visited_node)
+
+    return component_count
+
+
+def explore(graph, src, visited_node):
+    if src in visited_node:
+        return 0
+
+    visited_node.append(src)
+
+    for neighbor in graph[src]:
+        explore(graph, neighbor, visited_node)
+
+    return 1
+
+
+graph = {
+    '3': [],
+    '4': ['6'],
+    '6': ['4', '5', '7', '8'],
+    '8': ['6'],
+    '7': ['6'],
+    '5': ['6'],
+    '1': ['2'],
+    '2': ['1'],
+
+}
+
+print(has_path_dfs(graph))
+print(has_path_recursive_dfs(graph))
